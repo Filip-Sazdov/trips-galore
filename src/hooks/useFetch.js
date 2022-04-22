d992a5a3ee33cac46851578ea0a3fe7385a11c49
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-04-21 23:08:52 -0400

    add AbortController to make hook more resilient

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,11 +7,12 @@ export const useFetch = (url) => {
 
 
   useEffect(() => {
+    const controller = new AbortController()
 
     const fetchData = async () => {
       setIsLoading(true)
       try {
-        const response = await fetch(url)
+        const response = await fetch(url, { signal: controller.signal }) //fetch will now throw an AbortError if component unmounts while we are fetching data, and the catch block will catch it.
         if (!response.ok) {
           throw new Error(response.statusText) // if certain error occurs and response is not ok, this will throw the statusText down to the catch block where err.message will now be response.statusText
         }
@@ -21,16 +22,20 @@ export const useFetch = (url) => {
         setError(null)
 
       } catch (err) {  // this only catches network errors, like not getting a response due to lack of internet, will not throw error if request params or address are wrong!
-        setIsLoading(false)
-        setError('Could not fetch data!')
-        console.log(err.message)
-
+        if (err.name === "AbortError") {
+          console.log('Fetching of data was aborted!')
+        } else {
+          setIsLoading(false)
+          setError('Could not fetch data!')
+          console.log(err.message)
+        }
       }
-
-
     }
-
     fetchData()
+
+    return () => {  //this is a cleanup function which makes sure no errors occur if calling component gets unmounted while fetching data. This will abort the fetch which takes the AbortController signal as an argument.
+      controller.abort()
+    }
   }, [url])
 
   return { data, isLoading, error }
